013a89ebf41a7308cde65d8700787762bcbaeeea
ev_b <rainbowviola3@gmail.com>
2025-03-05 19:40:29 +0300

    3 task

--- main.py
+++ main.py
@@ -9,5 +9,20 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route("/training/<prof>")
+def train(prof):
+    return render_template('prof2.html', prof=prof)
+
+
+@app.route("/list_prof/<typ>")
+def profs(typ):
+    professions = ["инженер-исследователь", "пилот", "строитель", "экзобиолог",
+                   "врач", "инженер по терраформированию", "климатолог",
+                   "специалист по радиационноц защите", "астроеголог", "гляциолог",
+                   "инженер жизнеобеспечения", "метеоролог", "оператор марсохода",
+                   "штурман", "пилот дронов"]
+    return render_template("profs.html", professions=professions, typ=typ)
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8081)
